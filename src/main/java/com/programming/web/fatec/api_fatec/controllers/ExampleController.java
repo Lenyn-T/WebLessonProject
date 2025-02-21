029bb517fb86b5aadc75cbc199891eab74f955a8
Lenyn-T <lenyn.taka@gmail.com>
2025-02-20 22:48:33 -0300

    changed hello to default value and added variable to accept other string than hello

--- src/main/java/com/programming/web/fatec/api_fatec/controllers/ExampleController.java
+++ src/main/java/com/programming/web/fatec/api_fatec/controllers/ExampleController.java
@@ -1,14 +1,22 @@
 package com.programming.web.fatec.api_fatec.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
     
-    @GetMapping("")
-    public String HelloWorld(){
+    @GetMapping
+    public String HelloWorldDefault(){
         return "hello";
+    }
+
+    @GetMapping("{name}")
+    public String HelloWorld(@PathVariable Optional<String> name){
+        return name.isPresent() ? name.get() : "default";
     }
 
 }
