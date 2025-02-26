c1b3642464f3f04da5ec55f2c4fc8c6a95dbffba
Lenyn-T <lenyn.taka@gmail.com>
2025-02-26 19:35:20 -0300

    Exercise on the main page: age checking

--- src/main/java/com/programming/web/fatec/api_fatec/controllers/ExampleController.java
+++ src/main/java/com/programming/web/fatec/api_fatec/controllers/ExampleController.java
@@ -1,9 +1,6 @@
 package com.programming.web.fatec.api_fatec.controllers;
 
-import java.util.Optional;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,12 +8,23 @@ public class ExampleController {
     
     @GetMapping
     public String HelloWorldDefault(){
-        return "hello";
+        return "hello, welcome to this testing grounds.";
     }
 
-    @GetMapping("{name}")
-    public String HelloWorld(@PathVariable Optional<String> name){
-        return name.isPresent() ? name.get() : "default";
+    @GetMapping("{age}")
+    public String CheckAge(Integer age){
+        if (age <= 12 && age >= 0) {
+            return "The informed age is that of a CHILD.";
+        }
+        else if (age > 12 && age < 18) {
+            return "The informed age is that of a TEENAGER.";
+        }
+        else if (age < 0 && age > 130) {
+            return "Please inform an age of a living person.";
+        }
+        else{
+            return "The informed age is that of an ADULT.";
+        }
     }
 
 }
