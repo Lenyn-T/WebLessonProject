6836dcbdee3d27dda5fdb9a09c2f842395dc6420
Lenyn-T <lenyn.taka@gmail.com>
2025-03-13 22:30:53 -0300

    added get listing, delte and put functions

--- src/main/java/com/programming/web/fatec/api_fatec/controllers/ClienteController.java
+++ src/main/java/com/programming/web/fatec/api_fatec/controllers/ClienteController.java
@@ -1,6 +1,10 @@
 package com.programming.web.fatec.api_fatec.controllers;
 
 import com.programming.web.fatec.api_fatec.entities.Cliente;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +14,46 @@ import org.springframework.web.bind.annotation.*;
 public class ClienteController {
     private static final Logger logger = LoggerFactory.getLogger(ClienteController.class.getName());
 
+    private final List<Cliente> clientes = new ArrayList<>();
+    private Long idCount = 1L;
+
     //http://localhost:8080/api/cliente/criarCliente => POST
     @PostMapping("/criarCliente")
     public String CriarCliente(@RequestBody Cliente cliente){
+
+        cliente.setId(idCount++);
+        clientes.add(cliente);
+
         logger.info("Recebido JSON: Nome={}, Idade={}", cliente.getNome(), cliente.getIdade());
         return "O Cliente "+cliente.getNome()+" de idade "+cliente.getIdade()+" morador da rua: "+cliente.getLogradouro()+" foi criado.";
+    }
+
+    @GetMapping("/listarClientes")
+    public List<Cliente> ListarClientes() {
+        return clientes;
+    }
+
+    @GetMapping("/deletarCliente/{id}")
+    public String DeletarCliente(@PathVariable Long id){
+        for (Cliente cliente: clientes){
+            if (cliente.getId().equals(id)) {
+                clientes.remove(cliente);
+                return "Cliente "+cliente.getNome()+" removido.";
+            }
+        }
+
+        return "Cliente inexistente.";
+    }
+
+    @PutMapping("/alterarCliente/{id}/{nome}")
+    public String AlterarCliente(@PathVariable Long id, @PathVariable String nome){
+        for (Cliente cliente: clientes){
+            if (cliente.getId().equals(id)) {
+                cliente.setNome(nome);
+                return "Cliente id "+cliente.getId()+" alterado nome para: "+cliente.getNome();
+            }
+        }
+
+        return "Cliente inexistente.";
     }
 }
